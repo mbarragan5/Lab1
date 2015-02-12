46a20fdee0016b65ef3e62839f50c374d4934c71
Matthew Barragan <mbarragan@email.arizona.edu>
2015-02-12 16:51:52 -0700

    First Commit, Working Project

---
+++ Lab1part1.X/leds.c
@@ -0,0 +1,43 @@
+/*
+* File:   leds.c
+* Author:
+*
+* Created on December 27, 2014, 1:31 PM
+*/
+
+#include "p24FJ64GA002.h"
+#include "leds.h"
+
+
+void initLEDs()
+{
+
+    //TODO: Initialize the appropriate pins to work with the LEDs
+    //Pins 2 and 3;
+    TRISBbits.TRISB2 = 0; //LED1
+    TRISBbits.TRISB3 = 0; //LED2
+
+    LED1 = ON;
+    LED2 = OFF;
+}
+
+void initSW2()
+{
+    //TODO: Initialize the appropriate pins to work with the external switch using a
+    //change notification interrupt.
+     //pin 6
+
+    TRISBbits.TRISB6 = 1;
+
+    IFS1bits.CNIF = 0;
+
+    //Enable the overall change notification interrupt
+    IEC1bits.CNIE = 1;
+
+    /* Enable the change notification interrupt for the switch 1 connected to
+     * the development board. This corresponds to CN27 according to the PIC24F
+     * datasheet. The internal pull-up should not be used for the switch
+     * on the development board because one is already there.
+     */
+    CNEN2bits.CN27IE = 1;
+}
